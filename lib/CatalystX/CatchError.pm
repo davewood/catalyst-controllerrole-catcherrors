3a0d4678751e4388fd9bcf5874a519c0e90f2c2b
David Schmidt <mail@davidschmidt.at>
2014-08-22 12:40:42 +0200

    initial commit

---
+++ lib/CatalystX/CatchError.pm
@@ -0,0 +1,30 @@
+package CatalystX::CatchError;
+
+use Moose::Role;
+
+requires qw/ end catch_error /;
+
+# ABSTRACT: easy custom error handling in your controller.
+
+=head1 SYNOPSIS
+    package MyApp::Controller::Root;
+    with 'CatalystX::CatchError';
+
+    sub catch_error : Private {
+        my ($self, $c, @errors) = @_;
+        # stuff
+    }
+
+=head1 DESCRIPTION
+
+=cut
+
+before 'end' => sub {
+    my ( $self, $c ) = @_;
+    if ( $c->has_errors ) {
+        $c->forward( $self->action_for('catch_error'), [ @{ $c->error } ] );
+        $c->clear_errors;
+    }
+};
+
+1;
