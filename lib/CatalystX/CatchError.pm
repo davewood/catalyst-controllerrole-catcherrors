d096e6969163c3181b5a7673bce4fec219e87fd2
David Schmidt <mail@davidschmidt.at>
2014-08-26 14:55:07 +0200

    added DESCRIPTION

--- lib/CatalystX/CatchError.pm
+++ lib/CatalystX/CatchError.pm
@@ -16,6 +16,15 @@ requires qw/ end /;
     }
 
 =head1 DESCRIPTION
+By default L<Catalyst|Catalyst::Runtime> renders a HTML page if it encounters an error.
+One usecase where this is problematic is you create a REST API using
+L<Catalyst::Action::REST>. If you apply the CatalystX::CatchError role
+to your controller you can provide a method that receives an array of
+all errors that occurred during the request and do with them whatever
+you want.
+If you do not want to catch an error you have to rethrow it.
+CatalystX::CatchError passes a copy of the errors to your method and clears
+the original ones.
 
 =cut
 
