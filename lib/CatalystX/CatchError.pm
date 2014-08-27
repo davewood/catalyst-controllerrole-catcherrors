2f0d37a6da4885dfa5b0d06ee206fc6dee9ce5b5
David Schmidt <mail@davidschmidt.at>
2014-08-27 21:29:42 +0200

    documentation

--- lib/CatalystX/CatchError.pm
+++ lib/CatalystX/CatchError.pm
@@ -7,24 +7,26 @@ requires qw/ catch_errors end /;
 # ABSTRACT: custom error handling in your controller.
 
 =head1 SYNOPSIS
+
     package MyApp::Controller::Root;
+    use Moose;
+    BEGIN { extends 'Catalyst::Controller' }
     with 'CatalystX::CatchError';
 
-    sub catch_error : Private {
+    sub catch_errors : Private {
         my ($self, $c, @errors) = @_;
         # stuff
     }
 
 =head1 DESCRIPTION
-By default L<Catalyst|Catalyst::Runtime> renders a HTML page if it encounters an error.
-One usecase where this is problematic is you create a REST API using
-L<Catalyst::Action::REST>. If you apply the CatalystX::CatchError role
-to your controller you can provide a method that receives an array of
-all errors that occurred during the request and do with them whatever
-you want.
-If you do not want to catch an error you have to rethrow it.
-CatalystX::CatchError passes a copy of the errors to your method and clears
-the original ones.
+If an action throws and error the default behaviour of L<Catalyst|Catalyst::Runtime>
+is to render a default error page and set the response code to 500.
+One usecase where this is problematic is if you have a REST Controller using
+L<Catalyst::Action::REST>. If you apply the C<CatalystX::CatchError> role
+to your controller you can provide a C<catch_errors> action that receives an array of
+all errors that occurred during the request and do with them whatever you want.
+You can even rethrow the error. C<CatalystX::CatchError> passes a copy of the errors
+to your method and clears the original ones before calling C<catch_errors>.
 
 =cut
 
